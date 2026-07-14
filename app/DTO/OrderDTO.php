3d17f428521a5ba6d573085c6da69b73685435ce
Dmitriy <maxpower2877@gmail.com>
2026-07-15 01:47:45 +0500

    006|corrected Models

--- app/DTO/OrderDTO.php
+++ app/DTO/OrderDTO.php
@@ -1,1 +1,27 @@
 <?php
+
+namespace App\DTO;
+
+readonly class OrderDTO
+{
+    public function __construct(
+        public string $gNumber,
+        public string $date,
+        public string $lastChangeDate,
+        public string $supplierArticle,
+        public string $techSize,
+        public int $barcode,
+        public float $totalPrice,
+        public int $discountPercent,
+        public string $warehouseName,
+        public string $oblast,
+        public ?int $incomeId,
+        public ?string $odid,
+        public int $nmId,
+        public string $subject,
+        public string $category,
+        public string $brand,
+        public bool $isCancel,
+        public ?string $cancelDt,
+    ) {}
+}
