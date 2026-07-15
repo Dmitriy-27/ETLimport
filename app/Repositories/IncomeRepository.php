460f210ff0ea04c03d66d791410a4ccc0654802b
Dmitriy <maxpower2877@gmail.com>
2026-07-15 14:55:24 +0500

    017|Created Repository income.

---
+++ app/Repositories/IncomeRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Income;
+use App\Dto\IncomeDto;
+class IncomeRepository
+{
+    public function create(IncomeDTO $incomeDTo): Income
+    {
+        return Income::create([
+            'income_id' => $incomeDTo->income_id,
+            'number' => $incomeDTo->number,
+            'date' => $incomeDTo->date,
+            'last_change_date' => $incomeDTo->lastChangeDate,
+            'supplier_article' => $incomeDTo->supplierArticle,
+            'tech_size' => $incomeDTo->techSize,
+            'barcode' => $incomeDTo->barcode,
+            'quantity' => $incomeDTo->quantity,
+            'total_price' => $incomeDTo->totalPrice,
+            'date_close' => $incomeDTo->dateClose,
+            'warehouse_name' => $incomeDTo->warehouseName,
+            'nm_id' => $incomeDTo->nmId,
+        ]);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
