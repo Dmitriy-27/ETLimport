87dd2ac752ea859e4dc456879d7f3f05b1868737
Dmitriy <maxpower2877@gmail.com>
2026-07-15 14:48:27 +0500

    016|Created DTO income.

---
+++ app/DTO/IncomeDTO.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DTO;
+
+readonly class IncomeDTO
+{
+    public function __construct(
+        public int     $income_id,
+        public ?string $number,
+        public string  $date,
+        public string  $lastChangeDate,
+        public string  $supplierArticle,
+        public string  $techSize,
+        public int     $barcode,
+        public int     $quantity,
+        public float   $totalPrice,
+        public ?string $dateClose,
+        public string  $warehouseName,
+        public int     $nmId,
+    )
+    {
+    }
+}
