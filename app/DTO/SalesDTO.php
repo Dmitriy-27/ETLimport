9f43649f38879cf6b2502198ebd9a5acaf583a5f
Dmitriy <maxpower2877@gmail.com>
2026-07-15 12:59:54 +0500

    010|Created DTO Sales

--- app/DTO/SalesDTO.php
+++ app/DTO/SalesDTO.php
@@ -2,7 +2,35 @@
 
 namespace App\DTO;
 
-class SalesDTO
+readonly class SalesDTO
 {
-
+    public function __construct(
+        public string $gNumber,
+        public string $date,
+        public string $lastChangeDate,
+        public string $supplierArticle,
+        public string $techSize,
+        public int $barcode,
+        public float $totalPrice,
+        public int $discountPercent,
+        public bool $isSupply,
+        public bool $isRealization,
+        public ?float $promoCodeDiscount,
+        public string $warehouseName,
+        public string $countryName,
+        public string $oblastOkrugName,
+        public string $regionName,
+        public ?int $incomeId,
+        public string $saleId,
+        public ?string $odid,
+        public ?int $spp,
+        public float $forPay,
+        public float $finishedPrice,
+        public float $priceWithDisc,
+        public int $nmId,
+        public string $subject,
+        public string $category,
+        public string $brand,
+        public ?bool $isStorno,
+    ) {}
 }
